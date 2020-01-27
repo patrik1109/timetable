c3da5d5df48e18caceebbb9b34b15ad72a94b3c2
Egor <patrikeevegor@gmail.com>
2020-01-27 17:20:40 +0200

    add confirm message

--- src/main/java/timetable/config/WebSecurityConfig.java
+++ src/main/java/timetable/config/WebSecurityConfig.java
@@ -16,16 +16,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/index","/showHall").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                    .authorizeRequests()
+                    .antMatchers("/", "/home").permitAll()
+                    .anyRequest().authenticated();
+            http
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+
+
     }
 
     @Bean
