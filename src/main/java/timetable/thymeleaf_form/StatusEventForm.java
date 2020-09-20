ce2e6629f2573d939fa11377c733b9165becdecd
Egor <patrikeevegor@gmail.com>
2020-09-20 16:03:57 +0300

    statuseventform

---
+++ src/main/java/timetable/thymeleaf_form/StatusEventForm.java
@@ -0,0 +1,25 @@
+package timetable.thymeleaf_form;
+
+public class StatusEventForm {
+
+
+    public Integer estatus;
+    public int idEvent;
+
+    public int getIdEvent() {
+        return idEvent;
+    }
+
+    public void setIdEvent(int idEvent) {
+        this.idEvent = idEvent;
+
+    }
+
+    public Integer getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(Integer estatus) {
+        this.estatus = estatus;
+    }
+}
